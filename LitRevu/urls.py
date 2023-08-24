d605e388e09b7c912f5cdb8394a7a0d555391127
Tsuplige <thomassupligeon@gmail.com>
2023-08-24 22:33:02 +0200

    preparation : des models, des template et des path pour app

--- LitRevu/urls.py
+++ LitRevu/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     path('signup/', authentication.views.signup_page, name='signup'),
 
     path('home/', app.views.home, name='home'),
+
+    path('ticket/', app.views.ticket, name='ticket'),
+    path('ticket/<int:id>/change/', app.views.ticket_update, name='ticket-update'),
+
+    path('critique/', app.views.critique, name='critique'),
+    path('critique/<int:id>/change/', app.views.critique_update, name='critique-update'),
+
+    path('abonnements/', app.views.subscription, name='subscription'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,23 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     return render(request, 'app/home.html')
+
+
+def ticket(request):
+    return render(request, 'app/ticket.html')
+
+
+def ticket_update(request):
+    return render(request, 'app/ticket_update.html')
+
+
+def critique(request):
+    return render(request, 'app/critique.html')
+
+
+def critique_update(request):
+    return render(request, 'app/critique_update.html')
+
+
+def subscription(request):
+    return render(request, 'app/subscription.html')

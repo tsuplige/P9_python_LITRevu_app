97d1bac17aeff5fe2e11b6804b01bf09d5e87591
Tsuplige <thomassupligeon@gmail.com>
2023-08-25 00:54:31 +0200

    ajout des models de app

--- app/models.py
+++ app/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 
 class Ticket(models.Model):
-    # Your Ticket model definition goes here
-    pass
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048, blank=True)
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
 
 
 class Review(models.Model):
@@ -17,13 +21,18 @@ class Review(models.Model):
     body = models.CharField(max_length=8192, blank=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    time_created = models.DateTimeField(auto_now_add=True)
+    time_created = models.DateTimeField(auto_now_add=True)*
 
 
-# class UserFollows(models.Model):
-#     # Your UserFollows model definition goes here
+class UserFollows(models.Model):
+    # Your UserFollows model definition goes here
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
 
-#     class Meta:
-#         # ensures we don't get multiple UserFollows instances
-#         # for unique user-user_followed pairs
-#         unique_together = ('user', 'followed_user', )
+    class Meta:
+        # ensures we don't get multiple UserFollows instances
+        # for unique user-user_followed pairs
+        unique_together = ('user', 'followed_user', )
